85028079a1f51fc7ebd56a4e6c6cfcc7c5c5bc84
stefpolanco1 <78278343+stefpolanco1@users.noreply.github.com>
2021-07-18 19:51:11 -0400

    Added the Github repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Stefanie Polanco and is {" "}
+      <a href="https://github.com/stefpolanco1/weather-app-react"
+      target="_blank">
+        open-sourced on Github
+      </a>
+      </footer>
     </div>
   );
 }
