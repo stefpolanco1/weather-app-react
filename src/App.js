47c7a233c94be283441068855fd65f7166c3ce9a
stefpolanco1 <78278343+stefpolanco1@users.noreply.github.com>
2021-07-18 21:06:27 -0400

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Stefanie Polanco and is {" "}
-      <a href="https://github.com/stefpolanco1/weather-app-react"
-      target="_blank">
-        open-sourced on Github
-      </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Stefanie Polanco and is {" "}
+        <a href="https://github.com/stefpolanco1/weather-app-react"
+        target="_blank">
+          open-sourced on Github
+        </a>
+        </footer>
+      </div>
     </div>
   );
 }
